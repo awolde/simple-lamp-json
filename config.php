bff91c1cd9a92a40e2c08371603d52642fa29927
Aman Wolde <aman.wolde@mavenwave.com>
2019-03-01 21:27:46 -0600

    use env for db-host

--- config.php
+++ config.php
@@ -1,19 +1,8 @@
 <?php
 // Database connection parameters
-$db_hostname = "db";
+$db_hostname = $_ENV["DB_HOST"];
 $db_port     = 3306;
 $db_database = "employees";
 $db_username = "root";
 $db_password = "example";
-
-// Image upload options
-$storage_option = "hd";
-$hd_folder  = "uploads";
-
-// Simulate latency, in seconds
-$latency = 0;
-
-// Cache configuration
-$enable_cache = false;
-$cache_server = "dns-or-ip-of-memcached-server";
 ?>
